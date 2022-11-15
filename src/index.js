b8494e439bb18b5745a09b572990f7daa8662deb
Kormos Csanad <csanadsuli19@gmail.com>
2022-11-15 10:25:16 +0100

    Eltűntek a Syntax Errorok

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-import './style.css';
 
+
+
+
+
+console.log("Almafal loaded");
+let idezetek;
+
+idezetek.push(new Idezet("Annamária", "almafa"));
+
+class Idezet{
+    #name;
+    #quote;
+
+    constructor(name, quote){
+        this.#name = name;
+        this.#quote = quote;
+    }
+
+    getName(){
+        return this.#name
+    }
+    getQuote(){
+        return this.#quote;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.getElementById('osszesIdezetLista').value = "Almafa";
+    console.log("Almafa loaded");
+    async () => {
+        let response = await fetch('/quotes.json'); 
+        let eredmeny = await response.json();
+
+        console.log(response);
+        
+    }
+
+
+});
