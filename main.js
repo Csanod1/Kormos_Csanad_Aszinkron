c02c2020dc4db8f6c306543af29434d5a167e889
Csanod1 <Csanod1@users.noreply.github.com>
2022-11-15 09:25:45 +0000

    deploy: b8494e439bb18b5745a09b572990f7daa8662deb

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Almafal loaded"),(void 0).push(new class{#e;#t;constructor(e,t){this.#e=e,this.#t=t}getName(){return this.#e}getQuote(){return this.#t}}("Annamária","almafa")),document.addEventListener("DOMContentLoaded",(()=>{document.getElementById("osszesIdezetLista").value="Almafa",console.log("Almafa loaded")}))})();
